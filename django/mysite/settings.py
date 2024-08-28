da8f07c540a478114bab8630ee0cbe3ad63bc6a2
陳宣廷 <113078480+xuanting09@users.noreply.github.com>
2024-08-28 21:16:26 +0900

    8/28 bingo

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -18,6 +18,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'  # 登入成功後重定向到 'home'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
--- django/testttt/urls.py
+++ django/testttt/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import SomeProtectedView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('protected/', SomeProtectedView.as_view(), name='protected'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('index/', views.index_view, name='index'),  # 主页
     path('about/', views.about, name='about'),
@@ -13,4 +15,6 @@ urlpatterns = [
     path('subscribe/', views.subscribe, name='subscribe'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('search/cveid/', views.cve_search, name='cve_search'),
+    path('cve/<str:cve_id>/', views.cve_search, name='cve_search'),
 ]
